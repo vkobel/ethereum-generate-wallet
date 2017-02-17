78f204d6155495d0cee9a8650019f400672f1144
Vincent Kobel <v@klaat.com>
2017-02-17 14:49:00 +0100

    use sed as little as possible

--- ethereum-wallet-generator.sh
+++ ethereum-wallet-generator.sh
@@ -2,10 +2,10 @@
 keys=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout 2> /dev/null)
 
 # extract private key in hex format, removing newlines, leading zeroes and semicolon 
-priv=$(printf "%s\n" $keys | grep priv -A 3 | tail -n +2 | tr -d '\n' | sed 's/^00//; s/://g')
+priv=$(printf "%s\n" $keys | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
 
 # extract public key in hex format, removing newlines, leading '04' and semicolon 
-pub=$(printf "%s\n" $keys | grep pub -A 5 | tail -n +2 | tr -d '\n' | sed 's/^04//; s/://g')
+pub=$(printf "%s\n" $keys | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
 
 # get the keecak hash, removing the trailing ' -' and taking the last 40 chars
 # https://github.com/maandree/sha3sum
