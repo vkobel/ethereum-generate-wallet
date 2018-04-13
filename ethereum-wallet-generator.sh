1c73bbc8a5ebedf3da6fc4d239e7848cbd64d834
Vincent Kobel <v@klaat.com>
2018-04-13 15:31:04 +0200

    dumb fixing the length issue in bash script

--- ethereum-wallet-generator.sh
+++ ethereum-wallet-generator.sh
@@ -9,17 +9,23 @@ fi
 
 keys=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout 2> /dev/null)
 
-# extract private key in hex format, removing newlines, leading zeroes and semicolon 
+# extract private key in hex format, removing newlines, leading zeroes and semicolon
 priv=$(printf "%s\n" $keys | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
 
-# extract public key in hex format, removing newlines, leading '04' and semicolon 
+# make sure priv has correct length
+if [ ${#priv} -ne 64 ]; then
+    echo "length error"
+    exit
+fi
+
+# extract public key in hex format, removing newlines, leading '04' and semicolon
 pub=$(printf "%s\n" $keys | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
 
 # get the keecak hash, removing the trailing ' -' and taking the last 40 chars
 # https://github.com/maandree/sha3sum
-addr=0x$(echo $pub | lib/$arch/keccak-256sum -x -l | tr -d ' -' | tail -c 41) 
+addr=0x$(echo $pub | lib/$arch/keccak-256sum -x -l | tr -d ' -' | tail -c 41)
 
 echo 'Private key:' $priv
-echo 'Public key: ' $pub 
-echo 'Address:    ' $addr 
+echo 'Public key: ' $pub
+echo 'Address:    ' $addr
 
--- ethereum-wallet-generator_bulk.sh
+++ ethereum-wallet-generator_bulk.sh
@@ -25,13 +25,18 @@ if [ "$keysAmount" -eq "$keysAmount" ] 2>/dev/null; then #this checks if input i
 				# extract private key in hex format, removing newlines, leading zeroes and semicolon
 				priv=$(printf "%s\n" $keys | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
 
+                # make sure the private key has correct length
+                if [ ${#priv} -ne 64 ]; then
+                    continue
+                fi
+
 				# extract public key in hex format, removing newlines, leading '04' and semicolon
 				pub=$(printf "%s\n" $keys | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
 
 				# get the keecak hash, removing the trailing ' -' and taking the last 40 chars
 				# https://github.com/maandree/sha3sum
 				addr=0x$(echo $pub | lib/$arch/keccak-256sum -x -l | tr -d ' -' | tail -c 41)
-				
+
 				echo "$priv;$pub;$addr";
 			done
 	fi
